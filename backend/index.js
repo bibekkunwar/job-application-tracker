03fdaf1fd1c0579befea62bddd203d0a2db3f369
bibekkunwar <bibek.kunwar@nitv.com.np>
2026-04-28 22:54:40 +1000

    connect backend to supabase

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
+require('dotenv').config()
+const supabase = require('./config/supabase')
+
+
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.json({message: 'Server is running'});
-});
+// app.get('/', (req,res) => {
+//     res.json({message: 'Server is running'});
+// });
+
+//test routes
+
+app.get('/test-db', async (req, res) => {
+  const { data, error } = await supabase
+    .from('JobApplication')
+    .select('*')
+
+  if (error) return res.status(500).json({ error: error.message })
+  res.json({ data })
+})
 
 const PORT = process.env.PORT || 5000;
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function app() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-blue-500">
+            <h1 className="text-white text-4xl font-bold">Job Tracker</h1>
+
+        </div>
+    )
+}
+
+export default app;
