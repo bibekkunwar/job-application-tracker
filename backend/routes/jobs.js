e302a631e6648589e77ceccc574b37f8287ba9ab
bibekkunwar <bibek.kunwar@nitv.com.np>
2026-04-30 12:53:54 +1000

    add jobs REST API routes - Full CRUD

---
+++ backend/routes/jobs.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const router = express.Router();
+
+const supabase = require('../config/supabase.js');
+
+// get all the jobs 
+
+router.get('/', async(req, res) => {
+
+    try{
+
+        const {data,error} = await supabase
+            .from('job_application')
+            .select('*');
+
+            if(error)
+                throw error;
+
+            res.status(200).json(data)
+    }
+
+    
+
+    catch(error){
+        console.error('something went wrong', error.message);
+        res.status(500).json({error: error.message})
+    }
+
+});
+
+// end of get all job
+
+
+// start of post job application
+
+router.post('/', async(req, res) => {
+
+    
+
+    try{
+
+        const {user_id, company_name, role, status, platform, notes, job_url, date_applied } = req.body;
+
+        if(!company_name || !role || !status || !date_applied) {
+            return res.status(400).json({error: 'Input the required field'})
+        }
+
+        const {data,error} = await supabase
+            .from('job_application')
+            .insert([{ user_id:'00000000-0000-0000-0000-000000000000', company_name, role, status, platform, notes, job_url, date_applied }]).select();
+
+            if(error)
+                throw error;
+
+            res.status(201).json( {
+                message: 'Data inserted successfully',
+                data: data 
+            });
+    }
+
+    
+
+    catch(error){
+        console.error('something went wrong', error.message);
+        res.status(500).json({
+            error: 'An internal server error occured',
+            details: error.message
+        })
+    }
+
+});
+
+// end of post job application
+
+// start of get job by id
+
+router.get('/:id', async(req, res) => {
+
+    try{
+
+        const jobId =req.params.id
+
+        const {data,error} = await supabase
+            .from('job_application')
+            .select('*').eq('job_id', jobId );
+
+            if(error)
+                throw error;
+
+            if (data.length === 0)
+                return res.status(404).json({error: 'Incorrect job ID'})
+
+            res.status(200).json(data)
+    }
+
+    
+
+    catch(error){
+        console.error('something went wrong', error.message);
+        res.status(500).json({error: error.message})
+    }
+
+});
+
+// enf of get job by ID
+
+// start of put by id
+
+router.put('/:id', async(req,res) => {
+
+    try {
+
+        const jobId = req.params.id;
+        const {status, notes, platform, role } = req.body
+
+        const {data, error} = await supabase
+            .from('job_application')
+            .update({status, notes, platform, role}).eq('job_id', jobId).select();
+        
+        if (error)
+            throw error;
+
+        if(data.length === 0)
+            return res.status(404).json({error: 'job not found'})
+
+        res.status(200).json(data)
+    }
+
+    catch(error){
+        res.status(500).json({error: error.message})
+    }
+});
+
+// end of put by id
+
+// start of delete by id
+
+router.delete('/:id', async(req,res) => {
+    try {
+        const jobId = req.params.id;
+
+        const {data, error} = await supabase
+            .from('job_application')
+            .delete().eq('job_id', jobId).select();
+
+        if(error)
+            throw error
+
+        if(data.length===0){
+            return res.status(404).json({error: 'job not found'})
+        }
+
+        res.status(200).json('Job successfully deleted')
+
+    }
+
+    catch(error) {
+        res.status(500).json({error: error.message})
+    }
+});
+
+
+module.exports = router;
