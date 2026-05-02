fbdbdf61e9d160842add0cee69c0e2e75b3aaf4d
bibekkunwar <bibek.kunwar@nitv.com.np>
2026-05-02 21:22:00 +1000

    fir: CORS to use environment variable

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: process.env.FRONTEND_URL,
+  credentials: true
 }));
 
 app.use(express.json());
